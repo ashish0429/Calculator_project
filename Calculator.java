c3bd854b9bc83cb5fb774a533762d651d18d1640
ashish0429 <ashisharchd@gmail.com>
2025-12-17 13:39:28 +0530

    My first commit

--- Calculator.java
+++ Calculator.java
@@ -4,5 +4,6 @@ public class Calculator {
     public static void main(String[] args) {
         int a = 10, b = 5;
         System.out.println("Addition: " + (a + b));
+        System.out.println("Multiplication: " + (a * b));
     }
 }
